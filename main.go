a08ba9c2b1ec36a84b21a4d8a2494f2eaa892e80
naokita050 <49198895+naokita050@users.noreply.github.com>
2019-05-04 17:01:27 +0900

    LineBotでも原始人にする

--- main.go
+++ main.go
@@ -95,7 +95,8 @@ func main() {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					parsedMessage := getParsedWords(message.Text)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(parsedMessage)).Do(); err != nil {
 						log.Print(err)
 					}
 				}
